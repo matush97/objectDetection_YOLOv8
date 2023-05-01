3bedb36d46e5a3166f9ada7abafeb9b1438e7869
matush97 <hudym97@gmail.com>
2023-05-01 11:32:10 +0200

    Update test_model.py for finding result (not_hold, one_hand_hold, two_hand_hold)

--- test_model.py
+++ test_model.py
@@ -1,12 +1,14 @@
 # Importing the libraries
 from ultralytics import YOLO
 import cv2
-import numpy as np
 from shapely.geometry import box
 
 # Constants
 img_path = "test_photo/image1.jpg"
 path = "runs/detect/train/weights/best.pt"
+
+class_position = 5
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Download the model
 model = YOLO(path)
@@ -27,7 +29,6 @@ class_color = {
     2: (0, 255, 0)
 }
 
-
 # Drawing a bounding boxes - with name and percentage
 for bounding_box in bounding_boxes:
     x0 = bounding_box[0]
@@ -35,54 +36,54 @@ for bounding_box in bounding_boxes:
     y0 = bounding_box[1]
     y1 = bounding_box[3]
 
-    object_color = class_color.get(bounding_box[5])
-    object_name = class_indices.get(bounding_box[5])
+    object_color = class_color.get(bounding_box[class_position])
+    object_name = class_indices.get(bounding_box[class_position])
 
     start_point = (int(x0), int(y0))
     end_point = (int(x1), int(y1))
 
     cv2.rectangle(imageShow, start_point, end_point, color=object_color, thickness=2)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
     imageInfo = '%s (%.2f%%)' % (object_name, bounding_box[4])
     cv2.putText(imageShow, imageInfo, start_point, font, 2, object_color, 4, cv2.LINE_AA)
 
 # Hand and steering wheel overlap detection
 length_bounding_boxes = len(bounding_boxes)
-x = 0
 count_prediction = 0
 
-# for i in range(0, length_bounding_boxes):
-#     first_array = bounding_boxes[i]
-#     for j in range(x, length_bounding_boxes):
-#         second_array = bounding_boxes[j]
-#         # compare_arrays =
-#         if np.array_equal(first_array, second_array):
-#             continue
-#
-#         # area
-#         rectangle1 = box(first_array[0], first_array[1], first_array[2], first_array[3])
-#         rectangle2 = box(second_array[0], second_array[1], second_array[2], second_array[3])
-#
-#         intersection = rectangle1.intersection(rectangle2).area / rectangle1.union(rectangle2).area
-#
-#         print(intersection)
-#
-#         if intersection > 0:
-#             count_prediction += 1
-#     x += 1
+for i in range(0, length_bounding_boxes):
+    first_array = bounding_boxes[i]
 
-# Output
-output = "nedrzi"
-# if count_prediction == 1:
-#     output = "drzi s jednou rukou"
-# elif count_prediction == 2:
-#     output = "drzi s dvoma rukami"
+    # if there is no class for hand-hold
+    if first_array[class_position] != 0:
+        continue
+
+    for j in range(0, length_bounding_boxes):
+        second_array = bounding_boxes[j]
+
+        # if there is no class for steering-wheel
+        if second_array[class_position] != 2:
+            continue
+
+        # area
+        rectangle1 = box(first_array[0], first_array[1], first_array[2], first_array[3])
+        rectangle2 = box(second_array[0], second_array[1], second_array[2], second_array[3])
+
+        intersection = rectangle1.intersection(rectangle2).area / rectangle1.union(rectangle2).area
+
+        if intersection > 0:
+            count_prediction += 1
+
+output = "not_hold"
+if count_prediction == 1:
+    output = "one_hand_hold"
+elif count_prediction == 2:
+    output = "two_hand_hold"
 
 print(output)
 
-# Save an image to storage device
-# cv2.imwrite("example_with_bounding_boxes.jpg", imageShow)
+# Add title
+cv2.putText(imageShow, output, (50, 150), font, 2.5, (0, 0, 255), 4, cv2.LINE_AA)
 
 # Resize picture
 scale_percent = 40
@@ -90,6 +91,9 @@ width = int(imageShow.shape[1] * scale_percent / 100)
 height = int(imageShow.shape[0] * scale_percent / 100)
 dsize = (width, height)
 imageShow = cv2.resize(imageShow, dsize)
+
+# Save an image to storage device
+cv2.imwrite("example_with_bounding_boxes.jpg", imageShow)
 
 cv2.imshow('Photo', imageShow)
 cv2.waitKey(0)
